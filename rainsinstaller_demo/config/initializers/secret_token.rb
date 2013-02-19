585421ccfc46dc86e83a9a876034ad938ee979f5
Stephen Chen <bochencleveland@yahoo.com>
2013-02-18 20:15:57 -0500

    my first commit of my rails project

---
+++ rainsinstaller_demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RainsinstallerDemo::Application.config.secret_token = '3267233f5e0806d6f4ba3cd16b95fc924602f469b265b123b54226fae55a980b61130fae4acec103f954659994fb65804f61004b52d5e0e940834707af43ee39'
